87d5db50a7bf61897e975b974c4d5d48b5ea6e6d
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-08-07 19:47:49 +0400

    feat: add `EntityCurrencyConverter` for converting between Entity and DTO

---
+++ src/main/java/com/asalavei/currencyexchange/api/dao/converters/EntityCurrencyConverter.java
@@ -0,0 +1,39 @@
+package com.asalavei.currencyexchange.api.dao.converters;
+
+import com.asalavei.currencyexchange.api.dao.entities.EntityCurrency;
+import com.asalavei.currencyexchange.api.dto.Currency;
+
+/**
+ * Converter for converting between {@link EntityCurrency} and {@link Currency}
+ */
+public class EntityCurrencyConverter {
+    /**
+     * Converts the {@link EntityCurrency entity} to the {@link Currency}
+     *
+     * @param entity incoming the {@link EntityCurrency entity}
+     * @return the converted {@link Currency}
+     */
+    public Currency toDto(EntityCurrency entity) {
+        return Currency.builder()
+                .id(entity.getId())
+                .code(entity.getCode())
+                .fullName(entity.getFullName())
+                .sign(entity.getSign())
+                .build();
+    }
+
+    /**
+     * Converts the {@link Currency dto} to the {@link EntityCurrency}
+     *
+     * @param dto incoming the {@link Currency dto}
+     * @return the converted {@link EntityCurrency}
+     */
+    public EntityCurrency toEntity(Currency dto) {
+        return EntityCurrency.builder()
+                .id(dto.getId())
+                .code(dto.getCode())
+                .fullName(dto.getFullName())
+                .sign(dto.getSign())
+                .build();
+    }
+}
