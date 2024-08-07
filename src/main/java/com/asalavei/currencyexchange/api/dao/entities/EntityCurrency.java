48fd64eaee675359ff7fd1a8dc09e90414abdac6
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-08-07 13:14:48 +0400

    feat: add `EntityCurrency` and `EntityExchangeRate` for representing data stored in the database

---
+++ src/main/java/com/asalavei/currencyexchange/api/dao/entities/EntityCurrency.java
@@ -0,0 +1,96 @@
+package com.asalavei.currencyexchange.api.dao.entities;
+
+/**
+ * Entity representing currency data stored in the database
+ */
+public class EntityCurrency {
+    private Integer id;
+    private String code;
+    private String fullName;
+    private String sign;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getSign() {
+        return sign;
+    }
+
+    public void setSign(String sign) {
+        this.sign = sign;
+    }
+
+    /**
+     * Creates a new {@link Builder} instance for constructing {@link EntityCurrency} instances
+     *
+     * @return a new Builder instance
+     */
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    /**
+     * Builder for creating instances of the {@link EntityCurrency}
+     */
+    public static final class Builder {
+        private Integer id;
+        private String code;
+        private String fullName;
+        private String sign;
+
+        private Builder() {
+        }
+
+        public Builder id(Integer id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder code(String code) {
+            this.code = code;
+            return this;
+        }
+
+        public Builder fullName(String fullName) {
+            this.fullName = fullName;
+            return this;
+        }
+
+        public Builder sign(String sign) {
+            this.sign = sign;
+            return this;
+        }
+
+        public EntityCurrency build() {
+            EntityCurrency entityCurrency = new EntityCurrency();
+
+            entityCurrency.setId(id);
+            entityCurrency.setCode(code);
+            entityCurrency.setFullName(fullName);
+            entityCurrency.setSign(sign);
+
+            return entityCurrency;
+        }
+    }
+}
---
+++ src/main/java/com/asalavei/currencyexchange/api/dao/entities/EntityExchangeRate.java
@@ -0,0 +1,95 @@
+package com.asalavei.currencyexchange.api.dao.entities;
+
+import java.math.BigDecimal;
+
+/**
+ * Entity representing exchange rate data stored in the database
+ */
+public class EntityExchangeRate {
+    private Integer id;
+    private Integer baseCurrencyId;
+    private Integer targetCurrencyId;
+    private BigDecimal rate;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getBaseCurrencyId() {
+        return baseCurrencyId;
+    }
+
+    public void setBaseCurrencyId(Integer baseCurrencyId) {
+        this.baseCurrencyId = baseCurrencyId;
+    }
+
+    public Integer getTargetCurrencyId() {
+        return targetCurrencyId;
+    }
+
+    public void setTargetCurrencyId(Integer targetCurrencyId) {
+        this.targetCurrencyId = targetCurrencyId;
+    }
+
+    public BigDecimal getRate() {
+        return rate;
+    }
+
+    public void setRate(BigDecimal rate) {
+        this.rate = rate;
+    }
+
+    /**
+     * Creates a new {@link Builder} instance for constructing {@link EntityExchangeRate} instances
+     *
+     * @return a new Builder instance
+     */
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    /**
+     * Builder for creating instances of the {@link EntityExchangeRate}
+     */
+    public static final class Builder {
+        private Integer id;
+        private Integer baseCurrencyId;
+        private Integer targetCurrencyId;
+        private BigDecimal rate;
+
+        public Builder id(Integer id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder baseCurrencyId(Integer baseCurrencyId) {
+            this.baseCurrencyId = baseCurrencyId;
+            return this;
+        }
+
+        public Builder targetCurrencyId(Integer targetCurrencyId) {
+            this.targetCurrencyId = targetCurrencyId;
+            return this;
+        }
+
+        public Builder rate(BigDecimal rate) {
+            this.rate = rate;
+            return this;
+        }
+
+        public EntityExchangeRate build() {
+            EntityExchangeRate entityExchangeRate = new EntityExchangeRate();
+
+            entityExchangeRate.setId(id);
+            entityExchangeRate.setBaseCurrencyId(baseCurrencyId);
+            entityExchangeRate.setTargetCurrencyId(targetCurrencyId);
+            entityExchangeRate.setRate(rate);
+
+            return entityExchangeRate;
+        }
+    }
+}
