1039fb45b4aaa2e08811eeab7ac28de8ea790151
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-08-08 21:49:18 +0400

    feat: add `CrudService` interface, `BaseCrudService` abstract class and `CurrencyService` implementation with `findAll()` method

---
+++ src/main/java/com/asalavei/currencyexchange/api/services/BaseCrudService.java
@@ -0,0 +1,29 @@
+package com.asalavei.currencyexchange.api.services;
+
+import com.asalavei.currencyexchange.api.dbaccess.converters.EntityDtoConverter;
+import com.asalavei.currencyexchange.api.dbaccess.dao.BaseEntityDao;
+import com.asalavei.currencyexchange.api.dbaccess.entities.BaseEntity;
+import com.asalavei.currencyexchange.api.dto.BaseDto;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+public abstract class BaseCrudService<I extends Comparable<I>,
+        D extends BaseDto<I>,
+        E extends BaseEntity<I>,
+        C extends EntityDtoConverter<I, E, D>,
+        R extends BaseEntityDao<I, E>> implements CrudService<I, D> {
+
+    private final R entityDao;
+    private final C converter;
+
+    protected BaseCrudService(R entityDao, C converter) {
+        this.entityDao = entityDao;
+        this.converter = converter;
+    }
+
+    @Override
+    public Collection<D> findAll() {
+        return entityDao.findAll().stream().map(converter::toDto).collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/com/asalavei/currencyexchange/api/services/CrudService.java
@@ -0,0 +1,9 @@
+package com.asalavei.currencyexchange.api.services;
+
+import com.asalavei.currencyexchange.api.dto.BaseDto;
+
+import java.util.Collection;
+
+public interface CrudService<I extends Comparable<I>, D extends BaseDto<I>> {
+    Collection<D> findAll();
+}
