ce6e701d88a67d489a954bb54eaf6d637e5e5a43
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-08-08 12:19:53 +0400

    feat: serialize database result to JSON in `CurrencyServlet`

--- src/main/java/com/asalavei/currencyexchange/api/controllers/CurrencyServlet.java
+++ src/main/java/com/asalavei/currencyexchange/api/controllers/CurrencyServlet.java
@@ -1,18 +1,19 @@
 package com.asalavei.currencyexchange.api.controllers;
 
+import com.asalavei.currencyexchange.api.json.JsonCurrency;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CurrencyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        PrintWriter out = response.getWriter();
-
         try {
             Class.forName("org.postgresql.Driver");
         } catch (ClassNotFoundException e) {
@@ -21,19 +22,32 @@ public class CurrencyServlet extends HttpServlet {
 
         try {
             Connection connection = DriverManager.getConnection(
-                    "jdbc:postgresql://localhost:5432/currency-exchange",
+                    "jdbc:postgresql://localhost:5432/currency_exchange",
                     "postgres", ""
             );
 
             Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT code, full_name, sign from currencies");
+            ResultSet resultSet = statement.executeQuery("SELECT * from currencies");
+
+            List<JsonCurrency> currencies = new ArrayList<>();
 
             while (resultSet.next()) {
-                out.println(resultSet.getString("code"));
-                out.println(resultSet.getString("full_name"));
-                out.println(resultSet.getString("sign"));
-                out.println("\n");
+                JsonCurrency jsonCurrency = JsonCurrency.builder()
+                        .id(resultSet.getInt("id"))
+                        .code(resultSet.getString("code"))
+                        .fullName(resultSet.getString("full_name"))
+                        .sign(resultSet.getString("sign"))
+                        .build();
+
+                currencies.add(jsonCurrency);
             }
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            String jsonCurrencyAsString = objectMapper.writeValueAsString(currencies);
+
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().write(jsonCurrencyAsString);
 
             statement.close();
         } catch (SQLException e) {
