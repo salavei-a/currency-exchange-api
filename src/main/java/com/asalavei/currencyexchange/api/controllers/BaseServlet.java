2d85ee59749b6b90b4a06886f54eb872a7994d9a
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-08-13 12:54:58 +0400

    refactor: improve inheritors structure in `BaseServlet`

--- src/main/java/com/asalavei/currencyexchange/api/controllers/BaseServlet.java
+++ src/main/java/com/asalavei/currencyexchange/api/controllers/BaseServlet.java
@@ -1,5 +1,9 @@
 package com.asalavei.currencyexchange.api.controllers;
 
+import com.asalavei.currencyexchange.api.dto.BaseDto;
+import com.asalavei.currencyexchange.api.json.BaseJsonDto;
+import com.asalavei.currencyexchange.api.json.converters.JsonDtoConverter;
+import com.asalavei.currencyexchange.api.services.CrudService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletResponse;
@@ -7,8 +11,25 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-public abstract class BaseServlet extends HttpServlet {
-    private final ObjectMapper objectMapper = new ObjectMapper();
+public abstract class BaseServlet<I extends Comparable<I>,
+        J extends BaseJsonDto<I>,
+        D extends BaseDto<I>,
+        C extends JsonDtoConverter<I, J, D>,
+        S extends CrudService<I, D>> extends HttpServlet {
+
+    protected final S service;
+    protected final C converter;
+
+    protected final ObjectMapper objectMapper = new ObjectMapper();
+
+    protected BaseServlet() {
+        this.service = createService();
+        this.converter = createConverter();
+    }
+
+    protected abstract S createService();
+
+    protected abstract C createConverter();
 
     protected <T> void writeJsonResponse(HttpServletResponse response, int statusCode, String errorMessage, T responseObject) {
         try {
