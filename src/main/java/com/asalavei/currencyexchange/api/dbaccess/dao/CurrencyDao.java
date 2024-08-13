3c1ba728a579e6152816b748823a80b4c6d272e4
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-08-13 16:47:35 +0400

    chore: remove unnecessary methods in interfaces `CurrencyDao` and `ExchangeRateDao`

--- src/main/java/com/asalavei/currencyexchange/api/dbaccess/dao/CurrencyDao.java
+++ src/main/java/com/asalavei/currencyexchange/api/dbaccess/dao/CurrencyDao.java
@@ -2,17 +2,6 @@ package com.asalavei.currencyexchange.api.dbaccess.dao;
 
 import com.asalavei.currencyexchange.api.dbaccess.entities.EntityCurrency;
 
-import java.util.Collection;
-
 public interface CurrencyDao extends BaseEntityDao<Integer, EntityCurrency> {
-    @Override
-    EntityCurrency save(EntityCurrency entity);
-
-    @Override
-    Collection<EntityCurrency> findAll();
-
     EntityCurrency findById(Integer id);
-
-    @Override
-    EntityCurrency findByCode(String code);
 }
--- src/main/java/com/asalavei/currencyexchange/api/dbaccess/dao/ExchangeRateDao.java
+++ src/main/java/com/asalavei/currencyexchange/api/dbaccess/dao/ExchangeRateDao.java
@@ -2,15 +2,5 @@ package com.asalavei.currencyexchange.api.dbaccess.dao;
 
 import com.asalavei.currencyexchange.api.dbaccess.entities.EntityExchangeRate;
 
-import java.util.Collection;
-
 public interface ExchangeRateDao extends BaseEntityDao<Integer, EntityExchangeRate> {
-    @Override
-    EntityExchangeRate save(EntityExchangeRate entity);
-
-    @Override
-    Collection<EntityExchangeRate> findAll();
-
-    @Override
-    EntityExchangeRate findByCode(String code);
 }
