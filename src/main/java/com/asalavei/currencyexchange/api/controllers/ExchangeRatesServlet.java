fa70d2725c394e2f9c4c24a931df59df333e1824
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-08-15 17:29:49 +0400

    feat: implement POST request method in `ExchangeRatesServlet`

--- src/main/java/com/asalavei/currencyexchange/api/controllers/ExchangeRatesServlet.java
+++ src/main/java/com/asalavei/currencyexchange/api/controllers/ExchangeRatesServlet.java
@@ -1,16 +1,21 @@
 package com.asalavei.currencyexchange.api.controllers;
 
+import com.asalavei.currencyexchange.api.dbaccess.converters.EntityCurrencyConverter;
 import com.asalavei.currencyexchange.api.dbaccess.converters.EntityExchangeRateConverter;
 import com.asalavei.currencyexchange.api.dbaccess.dao.JdbcCurrencyDao;
 import com.asalavei.currencyexchange.api.dbaccess.dao.JdbcExchangeRateDao;
 import com.asalavei.currencyexchange.api.dto.ExchangeRate;
+import com.asalavei.currencyexchange.api.exceptions.CEAlreadyExists;
 import com.asalavei.currencyexchange.api.exceptions.CEDatabaseUnavailableException;
+import com.asalavei.currencyexchange.api.exceptions.CENotFoundException;
 import com.asalavei.currencyexchange.api.json.JsonExchangeRate;
 import com.asalavei.currencyexchange.api.json.converters.JsonExchangeRateConverter;
+import com.asalavei.currencyexchange.api.services.CurrencyService;
 import com.asalavei.currencyexchange.api.services.ExchangeRateService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.math.BigDecimal;
 import java.util.Collection;
 
 public class ExchangeRatesServlet extends BaseServlet<Integer, JsonExchangeRate, ExchangeRate, JsonExchangeRateConverter, ExchangeRateService> {
@@ -18,6 +23,8 @@ public class ExchangeRatesServlet extends BaseServlet<Integer, JsonExchangeRate,
     public ExchangeRatesServlet() {
         super(new ExchangeRateService(new JdbcExchangeRateDao(new JdbcCurrencyDao()), new EntityExchangeRateConverter()), new JsonExchangeRateConverter());
     }
+
+    private final CurrencyService currencyService = new CurrencyService(new JdbcCurrencyDao(), new EntityCurrencyConverter());
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) {
@@ -33,5 +40,36 @@ public class ExchangeRatesServlet extends BaseServlet<Integer, JsonExchangeRate,
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+        String baseCurrencyCode = request.getParameter("baseCurrencyCode");
+        String targetCurrencyCode = request.getParameter("targetCurrencyCode");
+        String rateParam = request.getParameter("rate");
+
+        if (baseCurrencyCode == null || baseCurrencyCode.isEmpty() ||
+            targetCurrencyCode == null || targetCurrencyCode.isEmpty() ||
+            rateParam == null || rateParam.isEmpty()) {
+            writeJsonResponse(response, HttpServletResponse.SC_BAD_REQUEST, "Required form field is missing.", null);
+            return;
+        }
+
+        try {
+            BigDecimal rate = new BigDecimal(rateParam);
+
+            ExchangeRate exchangeRate = ExchangeRate.builder()
+                    .baseCurrency(currencyService.findByCode(baseCurrencyCode))
+                    .targetCurrency(currencyService.findByCode(targetCurrencyCode))
+                    .rate(rate)
+                    .build();
+
+            JsonExchangeRate savedJsonExchangeRate = converter.toJsonDto(service.create(exchangeRate));
+            writeJsonResponse(response, HttpServletResponse.SC_CREATED, null, savedJsonExchangeRate);
+        } catch (NumberFormatException e) {
+            writeJsonResponse(response, HttpServletResponse.SC_BAD_REQUEST, "Invalid rate format.", null);
+        } catch (CENotFoundException e) {
+            writeJsonResponse(response, HttpServletResponse.SC_NOT_FOUND, e.getMessage(), null);
+        } catch (CEAlreadyExists e) {
+            writeJsonResponse(response, HttpServletResponse.SC_CONFLICT, e.getMessage(), null);
+        } catch (CEDatabaseUnavailableException e) {
+            writeJsonResponse(response, HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage(), null);
+        }
     }
 }
