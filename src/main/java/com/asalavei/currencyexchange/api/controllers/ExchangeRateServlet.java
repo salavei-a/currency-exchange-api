13cf83f18203d613cdf7c42f7fe22a065a55554f
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-08-16 15:50:08 +0400

    feat: implement PATCH request method in `ExchangeRateServlet` for update the exchange rate

--- src/main/java/com/asalavei/currencyexchange/api/controllers/ExchangeRateServlet.java
+++ src/main/java/com/asalavei/currencyexchange/api/controllers/ExchangeRateServlet.java
@@ -17,6 +17,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.Map;
 
 public class ExchangeRateServlet extends BaseServlet<Integer, JsonExchangeRate, ExchangeRate, JsonExchangeRateConverter, ExchangeRateService> {
 
@@ -25,6 +27,15 @@ public class ExchangeRateServlet extends BaseServlet<Integer, JsonExchangeRate, 
     }
 
     private final CurrencyService currencyService = new CurrencyService(new JdbcCurrencyDao(), new EntityCurrencyConverter());
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if ("PATCH".equalsIgnoreCase(request.getMethod())) {
+            this.doPatch(request, response);
+        } else {
+            super.service(request, response);
+        }
+    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) {
@@ -52,15 +63,40 @@ public class ExchangeRateServlet extends BaseServlet<Integer, JsonExchangeRate, 
         }
     }
 
-    @Override
-    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if ("PATCH".equalsIgnoreCase(request.getMethod())) {
-            this.doPatch(request, response);
-        } else {
-            super.service(request, response);
-        }
-    }
-
     protected void doPatch(HttpServletRequest request, HttpServletResponse response) {
+        String pathInfo = request.getPathInfo();
+        Map<String, String> params = getFormParameters(request);
+        String rateParam = params.get("rate");
+
+        if (pathInfo == null || "/".equals(pathInfo) || pathInfo.length() != 7) {
+            writeJsonResponse(response, HttpServletResponse.SC_BAD_REQUEST,
+                    "Currency pair codes are missing in the URL request. The code for each currency in the pair must contain 3 characters.",
+                    null);
+            return;
+        }
+
+        if (rateParam == null || rateParam.isEmpty()) {
+            writeJsonResponse(response, HttpServletResponse.SC_BAD_REQUEST,
+                    "Rate value is missing.",
+                    null);
+            return;
+        }
+
+        try {
+            BigDecimal rate = new BigDecimal(rateParam);
+            int idBaseCurrency = currencyService.getIdByCode(pathInfo.substring(1, 4));
+            int idTargetCurrency = currencyService.getIdByCode(pathInfo.substring(4, 7));
+
+            ExchangeRate dtoExchangeRate = service.update(rate, idBaseCurrency, idTargetCurrency);
+            JsonExchangeRate jsonExchangeRate = converter.toJsonDto(dtoExchangeRate);
+
+            writeJsonResponse(response, HttpServletResponse.SC_OK, null, jsonExchangeRate);
+        } catch (NumberFormatException e) {
+            writeJsonResponse(response, HttpServletResponse.SC_BAD_REQUEST, "Invalid rate format.", null);
+        } catch (CENotFoundException e) {
+            writeJsonResponse(response, HttpServletResponse.SC_NOT_FOUND, e.getMessage(), null);
+        } catch (CEDatabaseUnavailableException e) {
+            writeJsonResponse(response, HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage(), null);
+        }
     }
 }
