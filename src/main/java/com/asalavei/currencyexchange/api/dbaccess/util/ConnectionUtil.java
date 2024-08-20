bd3f8b4ba5add4b5d9f450c3b8be259deca24878
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-08-20 22:43:31 +0400

    refactor: add system variable and default value for `POSTGRES_USER` and `POSTGRES_PASSWORD`

--- src/main/java/com/asalavei/currencyexchange/api/dbaccess/util/ConnectionUtil.java
+++ src/main/java/com/asalavei/currencyexchange/api/dbaccess/util/ConnectionUtil.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class ConnectionUtil {
     private static final String URL = "jdbc:postgresql://localhost:4679/currency_exchange";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "";
+    private static final String USER = System.getenv("POSTGRES_USER") != null ? System.getenv("POSTGRES_USER") : "admin";
+    private static final String PASSWORD = System.getenv("POSTGRES_PASSWORD") != null ? System.getenv("POSTGRES_PASSWORD") : "admin";
 
     static {
         try {
