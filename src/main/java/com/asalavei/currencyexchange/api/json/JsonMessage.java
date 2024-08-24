e334a11072edc96de2b2b369465d0c403c53ea33
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-08-24 19:47:04 +0400

    feat: add `JsonMessage`

---
+++ src/main/java/com/asalavei/currencyexchange/api/json/JsonMessage.java
@@ -0,0 +1,14 @@
+package com.asalavei.currencyexchange.api.json;
+
+public class JsonMessage {
+
+    private String message;
+
+    public JsonMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
