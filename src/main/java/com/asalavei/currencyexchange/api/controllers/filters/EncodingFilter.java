ebd5ce90b5d22e88d545b0c9f2760fb8d20fe25d
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-08-27 22:09:18 +0400

    feat: implement `EncodingFilter`

---
+++ src/main/java/com/asalavei/currencyexchange/api/controllers/filters/EncodingFilter.java
@@ -0,0 +1,21 @@
+package com.asalavei.currencyexchange.api.controllers.filters;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class EncodingFilter extends HttpFilter {
+
+    @Override
+    protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json");
+
+        super.doFilter(request, response, chain);
+    }
+}
