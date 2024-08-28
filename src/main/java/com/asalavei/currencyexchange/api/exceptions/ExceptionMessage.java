20e852c20cd1ece7b17005a8d3ff76ef2cdb0afb
Alex Salavei <aliakseisalavei.contact@gmail.com>
2024-08-28 12:16:20 +0400

    chore: style format `ExceptionMessage`

--- src/main/java/com/asalavei/currencyexchange/api/exceptions/ExceptionMessage.java
+++ src/main/java/com/asalavei/currencyexchange/api/exceptions/ExceptionMessage.java
@@ -6,14 +6,15 @@ public class ExceptionMessage {
     public static final String INPUT_DATA_MISSING = "Required input data is missing: %s";
     public static final String INPUT_DATA_INVALID_OR_MISSING = "Required input data is invalid or missing: %s";
 
+    public static final String ERROR_WRITING_RESPONSE = "Error writing response for status code: {}";
+    public static final String ERROR_PROCESSING_REQUEST = "An error occurred while processing the request";
+    public static final String ERROR_PROCESSING_REQUEST_TO_DATABASE = ERROR_PROCESSING_REQUEST + " to the database";
+
     public static final String CURRENCY_NOT_FOUND = "Currency with code %s not found";
     public static final String EXCHANGE_RATE_NOT_FOUND = "Exchange rate %s/%s not found";
     public static final String ALREADY_EXISTS = "Failed to save: %s already exists";
-    public static final String EXCHANGE_FAILED = "Exchange operation failed. Cause: %s";
     public static final String FAILED_OPERATION = "Failed to %s %s";
-    public static final String ERROR_PROCESSING_REQUEST = "An error occurred while processing the request";
-    public static final String ERROR_PROCESSING_REQUEST_TO_DATABASE = ERROR_PROCESSING_REQUEST + " to the database";
-    public static final String ERROR_WRITING_RESPONSE = "Error writing response for status code: {}";
+    public static final String EXCHANGE_FAILED = "Exchange operation failed. Cause: %s";
 
     private ExceptionMessage() {
     }
